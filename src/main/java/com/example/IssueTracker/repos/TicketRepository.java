6ee485ddb1a76325a5e4bb85775b7693f4c43aa9
Matt1218 <welfleym@gmail.com>
2022-08-11 17:04:23 -0400

    fixed gradle wrapper

--- src/main/java/com/example/IssueTracker/repos/TicketRepository.java
+++ src/main/java/com/example/IssueTracker/repos/TicketRepository.java
@@ -1,6 +1,12 @@
 package com.example.IssueTracker.repos;
 
-public interface TicketRepository {
+
+import com.example.IssueTracker.models.TicketResource;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends CrudRepository<TicketResource, Long> {
 
 
 }
---
+++ src/main/java/com/example/IssueTracker/services/TicketService.java
@@ -0,0 +1,7 @@
+package com.example.IssueTracker.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+}
