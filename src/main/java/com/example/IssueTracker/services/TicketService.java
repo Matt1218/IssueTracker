23d18ef00d017bb3213256621152da39f51ceb88
Matt1218 <welfleym@gmail.com>
2022-08-11 18:47:37 -0400

    repo and start of service

--- src/main/java/com/example/IssueTracker/services/TicketService.java
+++ src/main/java/com/example/IssueTracker/services/TicketService.java
@@ -1,7 +1,14 @@
 package com.example.IssueTracker.services;
 
+import com.example.IssueTracker.repos.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TicketService {
+
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    
 }
