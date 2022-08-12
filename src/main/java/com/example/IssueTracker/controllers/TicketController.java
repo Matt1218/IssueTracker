dbeae1e23814c3589c63143c2e503c4a4c0ccd2b
Matt1218 <welfleym@gmail.com>
2022-08-12 18:17:56 -0400

    ticket service tied in with controller

--- src/main/java/com/example/IssueTracker/controllers/TicketController.java
+++ src/main/java/com/example/IssueTracker/controllers/TicketController.java
@@ -2,21 +2,30 @@ package com.example.IssueTracker.controllers;
 
 import com.example.IssueTracker.models.TicketResource;
 import com.example.IssueTracker.repos.TicketRepository;
+import com.example.IssueTracker.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+
 import java.util.Collection;
 
 
 @RestController
 public class TicketController {
 
-    @Resource
-    private TicketRepository ticketRepository;
+
+    @Autowired
+    private TicketService ticketService;
 
     @GetMapping("/api/tickets")
     public Collection<TicketResource> getAllTickets(){
-        return (Collection<TicketResource>) ticketRepository.findAll();
+        return ticketService.getAllTickets();
+    }
+
+    @GetMapping("/api/ticketByName/{name}")
+    public TicketResource findTicketByName(@PathVariable String name){
+        return ticketService.getTicketByName(name);
     }
 }
