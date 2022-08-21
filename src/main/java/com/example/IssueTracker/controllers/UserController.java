0ab79cc8b972e60204259275f734e10ccd58e297
Matt1218 <welfleym@gmail.com>
2022-08-21 13:39:10 -0400

    user controller/populator

--- src/main/java/com/example/IssueTracker/controllers/UserController.java
+++ src/main/java/com/example/IssueTracker/controllers/UserController.java
@@ -1,9 +1,9 @@
 package com.example.IssueTracker.controllers;
 
+import com.example.IssueTracker.models.UserResource;
 import com.example.IssueTracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -11,4 +11,25 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @GetMapping("/login")
+    private UserResource getCurrentUser(@RequestBody UserResource userResource) {
+        return userService.getUser(userResource);
+    }
+
+    @GetMapping("login/{username}/{password}")
+    private boolean findUserByUsername(@PathVariable String username, @PathVariable String password) {
+        return userService.getUserByUsername(username, password);
+    }
+
+    @PostMapping("/createUser")
+    private boolean addUser(@RequestBody UserResource userResource) {
+        boolean userExists = userService.findUserByUsername(userResource.getUsername());
+        if (userExists) {
+            System.out.println("Username already exists");
+            return false;
+        }
+        userService.saveUser(userResource);
+        return true;
+    }
 }
